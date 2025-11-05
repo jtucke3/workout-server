e073264b685b317390726a99b5e67177b1773f87
MarchedGarlic <joey61904@hotmail.com>
2025-11-04 20:15:48 -0600

    Added ChangeProfileDetailsWebVeo class

---
+++ src/main/java/com/jtucke3/workoutapi/webVo/user/ChangeProfileDetailsWebRequestVo.java
@@ -0,0 +1,27 @@
+public class UserProfileWVO {
+    private String username;
+    private String email;
+
+    public UserProfileWVO() {}
+
+    public UserProfileWVO(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
